fdb63f8052d1451ec4beef8e8971c72333583109
Paolo Ruiz <pjruiz15@cs.utexas.edu>
2013-11-21 18:46:39 -0600

    Commit.

--- Mep.scala
+++ Mep.scala
@@ -1,11 +1,4 @@
-/**
-    #######        ##      ##    ##    ######     ####     ######     ##   ##
-    ##    ##      ####      ##  ##    ##           ##     ##    ##    ##  ##
-    #######      ##  ##       ##       ######      ##     ##          ####
-    ##    ##    ########      ##            ##     ##     ##    ##    ##  ##
-    #######    ##      ##     ##       ######     ####     ######     ##   ##
- */
-package fogus.baysick {
+package {
   import scala.collection.mutable.HashMap
   import scala.collection.mutable.Stack
 
@@ -45,7 +38,7 @@ package fogus.baysick {
    * Thanks to Szymon Jachim for the motivation to do this.
    *
    */
-  class Baysick {
+  class Mep {
     abstract sealed class BasicLine
     case class PrintString(num: Int, s: String) extends BasicLine
     case class PrintResult(num:Int, fn:Function0[String]) extends BasicLine
